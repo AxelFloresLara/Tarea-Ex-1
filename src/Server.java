85051fb02ccee6e344f77d61f5fd3d4523d4b24e
AxelFloresLara <axelsentado@gmail.com>
2022-03-07 18:39:16 -0600

    Primer Commit, se agregaron los objetos principales de la interfaz, junto a las bibliotecas a utilizar, adheridos tambien los comentarios respectivos

---
+++ src/Server.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Server {
+    /* Se deben definir los primeros objetos que van a componer la interfaz*/
+    
+    /* Jframe: es utilizado para generar la ventana inicial, la cual será personalizada posteriormente */
+    JFrame window_chat = null;
+    
+    /* Jtextfield: permite ingresar al operador del programa insertar caracteres mediante el teclado*/
+    JTextField message = null;
+    
+    /* Jbutton: es un botón que tiene como objetivo interactuar con él para realizar una accion determinada */
+    JButton send_button = null;
+    
+    /* Jpanel: es un contenedor de componentes, botones, campos de texto etc...*/
+    JPanel stock_chat = null;
+    JPanel stock_send_button = null;
+    
+
+    public static void main(String [] args){
+
+    }    
+} 
