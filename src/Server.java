bf94185d8b6be3f85c90ff52021359a135eb4219
AxelFloresLara <axelsentado@gmail.com>
2022-03-07 19:40:30 -0600

    Se añadió una nueva libreria y los primeros parametros a los metodos

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+import java.swing.*;
 import javax.swing.*;
 
 public class Server {
@@ -24,10 +25,11 @@ public class Server {
     }
     /* Metodo para interfaz */
     public void MakeInterface(){
-        window_chat = new JFrame();
-        message = new JTextField();
-        send_button = new JButton();
-        chat_area = new JTextArea();
+        window_chat = new JFrame("Server");
+        message = new JTextField(4);
+        send_button = new JButton("Send");
+        chat_area = new JTextArea(12, 14);
+        
 
     }
     public static void main(String [] args){
